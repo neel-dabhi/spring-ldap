a764c4dc7d6a065c82ff29c1b19f6a2660c0ef70
neelkanth <nl543396@dal.ca>
2021-04-13 23:22:22 -0300

    handled / by zero and null values

--- src/main/java/com/ndabhi/kangaroo/Service/CollisionService.java
+++ src/main/java/com/ndabhi/kangaroo/Service/CollisionService.java
@@ -8,14 +8,17 @@ import org.springframework.web.server.ResponseStatusException;
 public class CollisionService {
 
     public ReqResModel getCollisionPoint(ReqResModel reqResModel) {
+        checkNullObj(reqResModel);
+
         Integer x1 = reqResModel.getX1();
         Integer v1 = reqResModel.getV1();
         Integer x2 = reqResModel.getX2();
         Integer v2 = reqResModel.getV2();
 
         boolean isEqualVelocity = v1.equals(v2);
-        boolean isRemainderZero = (x2 - x1) % (v1 - v2) == 0;
+        boolean isRemainderZero = getRemainder(reqResModel);
         boolean isV1Greater = v1 > v2;
+
 
         if (isEqualVelocity) {
             boolean isSameStartPoint = x1.equals(x2);
@@ -65,5 +68,47 @@ public class CollisionService {
 
     private ResponseStatusException getResponseStatusException(String message) {
         return new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, message);
+    }
+
+    private boolean getRemainder(ReqResModel reqResModel){
+        Integer x1 = reqResModel.getX1();
+        Integer v1 = reqResModel.getV1();
+        Integer x2 = reqResModel.getX2();
+        Integer v2 = reqResModel.getV2();
+        boolean result;
+        try {
+            result  = (x2 - x1) % (v1 - v2) == 0;
+
+        }catch (ArithmeticException e){
+            throw getResponseStatusException(e.getMessage());
+        }
+        return result;
+
+    }
+
+    private void checkNullObj(ReqResModel reqResModel){
+        Integer x1 = reqResModel.getX1();
+        Integer v1 = reqResModel.getV1();
+        Integer x2 = reqResModel.getX2();
+        Integer v2 = reqResModel.getV2();
+
+        if (isNull(x1)){
+            throw getResponseStatusException("x1 can't be null");
+        }else if (isNull(v1)){
+            throw getResponseStatusException("v1 can't be null");
+        }else if (isNull(x2)){
+            throw getResponseStatusException("x2 can't be null");
+        }else if (isNull(v2)){
+            throw getResponseStatusException("v2 can't be null");
+        }
+    }
+
+    private boolean isNull(Object o){
+        if (null == o)
+        {
+            return true;
+        }else {
+            return false;
+        }
     }
 }
